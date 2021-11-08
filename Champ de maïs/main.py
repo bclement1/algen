e5fa8adc58bbd39b7b5a1ea822656461bb071f98
Clément <clementboulay0@gmail.com>
2021-11-08 19:41:58 +0100

    Commit initial

---
+++ Champ de maïs/main.py
@@ -0,0 +1,97 @@
+# Algorithme génétique - sélection empirique dans un champ de maïs
+
+# Importation
+import random
+random.seed(3)
+from random import randint, random
+import numpy as np
+
+# Définition des fonctions principales
+def individual(min, max):
+    # Un individu est un épi de maïs, caractérisé par sa position 2D et son rendement
+    return randint(min, max)
+
+def population(min, max, lx, ly):
+    # La population est un champ constitué d'épis de maïs
+    # lx, ly: longueur et largeur du champ, respectivement
+    pop = np.array([[individual(min, max) for i in range(lx)] for j in range(ly)], dtype = int)
+    return pop
+
+def grade(pop, max, lx, ly):
+    # Calcul de la performance globale du champ à une génération donnée
+    total_yield = 0
+    number_of_indiv = lx*ly
+    for i in range(lx):
+        for j in range(ly):
+            ind_yield = pop[i][j]
+            total_yield += ind_yield
+    field_fitness = (lx*ly*max - total_yield)/(lx*ly*max)*100 # écart relatif au champ optimal (en pourcentage)
+    return field_fitness
+
+def evolve(pop, min, max, lx, ly, retain_length = 0.1, random_select = 0.2, mutate = 0.1, verbose = 0):
+    """
+    Cette fonction prend en entrée les paramètres :
+    pop : la population au tour N
+    min, max : les valeurs minimale et maximale du rendement possibles pour un épi
+    lx, ly : les dimensions du champ
+    retain_length : la proportion des meilleurs épis à garder d'une génération à l'autre
+    random_select : la proportion d'épis à retenir d'un tour à l'autre (indépendamment de leur rendement)
+    mutate : le taux de mutation (évolution aléatoire du rendement) chez la population
+    eps : paramètre de croisement
+    """
+    # Classement des individus
+    grades = []
+    for i in range(lx):
+        for j in range(ly):
+            ind_yield = pop[i][j]
+            grades.append([ind_yield, i, j]) # l'évaluation des individus se fait directement via leur rendement
+    f = lambda x : x[0]
+    ranking = sorted(grades, key = f, reverse = True) # classement des épis par ordre décroissant de rendement
+    retain_length = int(len(grades)*retain_length)
+    # On garde une proportion des meilleurs individus pour peupler la prochaine génération
+    parents = ranking[:retain_length]
+    if verbose == 1:
+        print(len(parents), " épis ont été sélectionnés comme les plus performants.")
+    # Ajout d'épi de maïs au hasard parmi les autres individus pour la diversité génétique
+    for individual in ranking[retain_length:]:
+        if random_select > random():
+            parents.append(individual)
+    if verbose == 1:
+        print(len(parents), " épis ont été sélectionnés après diversité génétique.")
+    # Mutation de certains parents au hasard
+    for individual in parents:
+        if mutate > random():
+            new_yield = randint(min, max)
+            individual[0] = new_yield
+    # Croisement des parents pour créer la nouvelle génération
+    parents_length = len(parents)
+    parents_positions = [[indiv[1], indiv[2]] for indiv in parents]
+    children = []
+    # On itère sur les épis du champ
+    for i in range(lx):
+        for j in range(ly):
+            if not [i, j] in parents_positions:
+                # On choisit un épi père et un épi mère parmi les parents
+                ind_male = randint(0, parents_length - 1)
+                ind_female = randint(0, parents_length - 1)
+                while ind_male == ind_female:
+                    # Il faut que les épis parents soient différents
+                    ind_male = randint(0, parents_length - 1)
+                    ind_female = randint(0, parents_length - 1)
+                # Puis on hybride les parents pour créer un enfant épi de maïs :)
+                male = parents[ind_male]
+                female = parents[ind_female]
+                if male[0] >= female[0]:
+                    child_yield = randint(female[0], male[0])
+                    child = [child_yield, i, j]
+                elif female[0] > male[0]:
+                    child_yield = randint(male[0], female[0])
+                    child = [child_yield, i, j]
+                # Que l'on l'ajoute aux enfants
+                children.append(child)    
+    # Et enfin on ajoute les hybridés aux parents de la prochaine génération
+    parents.extend(children)
+    # Peuplement de la population par la nouvelle génération
+    for indiv in parents:
+        pop[indiv[1]][indiv[2]] = indiv[0] 
+    return pop
---
+++ Champ de maïs/test.py
@@ -0,0 +1,67 @@
+# Exemple d'exécution de l'algorithme génétique
+
+# Import functions from main.py
+from main import *
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from matplotlib.animation import FuncAnimation
+
+# Constantes
+min = 1
+max = 10
+lx = 100
+ly = 100
+number_of_gen = 100
+
+def show_fitness_history(min, max, lx, ly, number_of_gen, retain_length = 0.1, random_select = 0.1, mutate = 0.2):
+    # Création du champ
+    pop = population(min, max, lx, ly)
+    gr = grade(pop, max, lx, ly)
+    fitness_history = [grade(pop, max, lx, ly)]
+
+    # Itération sur les générations
+    for gen in range(number_of_gen):
+        pop = evolve(pop, min, max, lx, ly, retain_length = retain_length, random_select = random_select, mutate = mutate)
+        fitness_history.append(grade(pop, max, lx, ly))
+
+    Y = fitness_history
+    X = [i for i in range(len(Y))]
+    plt.title(
+    "Evolution of the population through generations. retain length = {} ; random select = {} ; mutation = {}"
+    .format(retain_length, random_select, mutate)
+    )
+    plt.xlabel("Gen.")
+    plt.ylabel("Average fitness")
+    plt.plot(X, Y, "k-x")
+    plt.show()
+
+# show_fitness_history(min, max, lx, ly, number_of_gen)
+
+def build_pop(min, max, lx, ly, number_of_gen, retain_length = 0.1, random_select = 0.1, mutate = 0.2):
+    tab = np.zeros(shape = (number_of_gen, lx, ly)) # là où on stocke les générations
+    # Création du champ
+    pop = population(min, max, lx, ly)
+
+    # Itération sur les générations
+    for gen in range(number_of_gen):
+        tab[gen] = pop
+        pop = evolve(pop, min, max, lx, ly, retain_length = retain_length, random_select = random_select, mutate = mutate)
+    return tab
+
+def return_pop(gen):
+    ax.cla()
+    sns.heatmap(tab[gen],
+                ax = ax,
+                cbar = True,
+                cmap = "Greens",
+                cbar_ax = cbar_ax,
+                vmin = tab.min(),
+                vmax = tab.max())
+
+
+tab = build_pop(min, max, lx, ly, number_of_gen, retain_length = 0.2, random_select = 0.05, mutate = 0.05)
+grid_kws = {'width_ratios': (0.9, 0.05), 'wspace': 0.2}
+fig, (ax, cbar_ax) = plt.subplots(1, 2, gridspec_kw = grid_kws, figsize = (12, 8))
+anim = FuncAnimation(fig = fig, func = return_pop, frames = number_of_gen, interval = 500)
+plt.show()
